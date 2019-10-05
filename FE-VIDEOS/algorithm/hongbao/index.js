5c30c0db20eea9bf6cf3c2e5c67955ae7097ad59
bblsnake <2925141441@qq.com>
2019-10-05 21:43:47 +0800

    hongbao

---
+++ FE-VIDEOS/algorithm/hongbao/index.js
@@ -0,0 +1,15 @@
+// total/num*2  抢的公平
+function hongbao(total, num) {
+  const arr = [];
+  let restAmount = total;//余下没领的钱
+  let restNum = num;//余下没领的人数
+  for (let i = 0; i < num - 1; i++) {
+    let amount = parseFloat(Math.random()*(restAmount/restNum)*2).toFixed(2);
+    restAmount -= amount;
+    restNum--;
+    arr.push(amount);
+  }
+  arr.push(restAmount.toFixed(2));
+  return arr;
+}
+console.log(hongbao(100, 10));

79dda98d3ae1db1ebcb251c450abab3cd0749caa
bblsnake <2925141441@qq.com>
2019-10-08 11:48:00 +0800

    menu

---
+++ menu/src/components/MenuList.vue
@@ -0,0 +1,36 @@
+/* eslint-disable */
+<template>
+<!-- 容器组件 -->
+  <Menu>
+    <template v-for="menu in data">
+      <MenuItem :key="menu.id" v-if="!menu.children">
+        {{menu.title}}
+      </MenuItem>
+      <ReSubMenu :data="menu" :key="menu.id" v-else>
+      </ReSubMenu>
+    </template>
+  </Menu>
+</template>
+
+<script>
+import Menu from './Menu'
+import MenuItem from './MenuItem'
+import ReSubMenu from './ReSubMenu'
+export default {
+  props: {
+    data: {
+      type: Array,
+      default: () => ([])
+    }
+  },
+  components: {
+    Menu,
+    MenuItem,
+    ReSubMenu
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
